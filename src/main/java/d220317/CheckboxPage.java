c491456155b62cf5a25f877f701402671c4641d0
kikimorella <kikimorella@gmail.com>
2022-03-31 20:54:02 +0200

    fix HW Checkboxes

--- src/main/java/d220317/CheckboxPage.java
+++ src/main/java/d220317/CheckboxPage.java
@@ -4,26 +4,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class CheckboxPage extends Page{
+import java.util.List;
+
+public class CheckboxPage extends Page {
 
     public CheckboxPage(WebDriver driver) {
         super(driver);
     }
+    /*
+        @FindBy(css = "input:nth-child(1)")
+        WebElement checkbox1;
 
-    @FindBy(css = "input:nth-child(1)")
-    WebElement checkbox1;
+        @FindBy(css = "input:nth-child(3)")
+        WebElement checkbox2;
+    */
+    @FindBy(xpath = "//form[@id='checkboxes']/input")
+    List<WebElement> checkboxesList;
+    /*
+        public void clickOn1checkbox() {
+            checkbox1.click();
+        }
 
-    @FindBy(css = "input:nth-child(3)")
-    WebElement checkbox2;
+        public void clickOn2checkbox() {
+            checkbox2.click();
+        }
 
-    @FindBy(id = "checkboxes")
-    WebElement passwordField;
+    public boolean isCheckboxNotSelected(int ind) { // при тесте мы выбираем сразу какой элемент нам нужен
+        String clicked = "checked";
+        return checkboxesList.get(ind).getAttribute(clicked).isEmpty(); // try catch exception
+    }
+    */
 
-    public void addElt1() {
-        checkbox1.click();
+    public boolean isCheckboxNotSelected(int ind) { // при тесте мы выбираем сразу какой элемент нам нужен
+        String clicked = "checked";
+        return checkboxesList.get(ind).getAttribute(clicked) != null; // try catch exception
     }
 
-    public void addElt2() {
-        checkbox2.click();
+    public void clickOnCheckbox(int ind) {
+        checkboxesList.get(ind).click();
     }
 }
+// https://stackoverflow.com/questions/20645013/how-to-verify-an-attribute-is-present-in-an-element-using-selenium-webdriver
--- src/test/java/d220317/Checkbox.java
+++ src/test/java/d220317/Checkbox.java
@@ -1,61 +1,53 @@
 package d220317;
+
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Before;
-import org.junit.After;
-import static org.junit.Assert.*;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
-import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 
-public class Checkbox extends BaseTest{
+public class Checkbox extends BaseTest {
 
     @Test
     public void checkbox1Click() { // оба выбраны
+        int checkboxIndex = 0;
         checkboxPage = homePage.goToCheckboxPage(); // driver.findElement(By.linkText("Checkboxes")).click();
-        checkboxPage.addElt1(); // driver.findElement(By.cssSelector("input:nth-child(1)")).click();
+        checkboxPage.clickOnCheckbox(checkboxIndex); // driver.findElement(By.cssSelector("input:nth-child(1)")).click();
+        assert (checkboxPage.isCheckboxNotSelected(checkboxIndex));
     }
 
     @Test
     public void checkbox2Click() { // оба не выбраны
+        int checkboxIndex = 1;
         checkboxPage = homePage.goToCheckboxPage(); // driver.findElement(By.linkText("Checkboxes")).click();
-        checkboxPage.addElt2(); // driver.findElement(By.cssSelector("input:nth-child(3)")).click();
+        checkboxPage.clickOnCheckbox(checkboxIndex); // driver.findElement(By.cssSelector("input:nth-child(3)")).click();
+        assert(!checkboxPage.isCheckboxNotSelected(checkboxIndex));
     }
 
     @Test
     public void clickCheckbox1and2() { // верхний даб нижний нет
         checkboxPage = homePage.goToCheckboxPage(); // driver.findElement(By.linkText("Checkboxes")).click();
-        checkboxPage.addElt1(); // driver.findElement(By.cssSelector("input:nth-child(1)")).click();
-        checkboxPage.addElt2(); // driver.findElement(By.cssSelector("input:nth-child(3)")).click();
+        checkboxPage.clickOnCheckbox(0); // driver.findElement(By.cssSelector("input:nth-child(1)")).click();
+        checkboxPage.clickOnCheckbox(1); // driver.findElement(By.cssSelector("input:nth-child(3)")).click();
+        assert(checkboxPage.isCheckboxNotSelected(0));
+        assert(!checkboxPage.isCheckboxNotSelected(1));
     }
 
     @Test
     public void clickCheckbox1twoTimes() { // нажимаем 2 раза на 1 элемент
+        int checkboxIndex = 0;
         checkboxPage = homePage.goToCheckboxPage(); // driver.findElement(By.linkText("Checkboxes")).click();
-        checkboxPage.addElt1(); // driver.findElement(By.cssSelector("input:nth-child(1)")).click();
-        checkboxPage.addElt1(); // driver.findElement(By.cssSelector("input:nth-child(1)")).click();
+        checkboxPage.clickOnCheckbox(checkboxIndex); // driver.findElement(By.cssSelector("input:nth-child(1)")).click();
+        checkboxPage.clickOnCheckbox(checkboxIndex); // driver.findElement(By.cssSelector("input:nth-child(1)")).click();
+        assert(!checkboxPage.isCheckboxNotSelected(checkboxIndex));
     }
 
     @Test
     public void clickCheckbox2twoTimes() { // нажимаем 2 раза на 2 элемент
+        int checkboxIndex = 1;
         checkboxPage = homePage.goToCheckboxPage(); // driver.findElement(By.linkText("Checkboxes")).click();
-        checkboxPage.addElt2(); // driver.findElement(By.cssSelector("input:nth-child(3)")).click();
-        checkboxPage.addElt2(); // driver.findElement(By.cssSelector("input:nth-child(3)")).click();
+        checkboxPage.clickOnCheckbox(checkboxIndex); // driver.findElement(By.cssSelector("input:nth-child(3)")).click();
+        checkboxPage.clickOnCheckbox(checkboxIndex); // driver.findElement(By.cssSelector("input:nth-child(3)")).click();
+        assert (checkboxPage.isCheckboxNotSelected(checkboxIndex));
     }
-
 }
